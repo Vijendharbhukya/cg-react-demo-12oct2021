45eac69fad02d68142fd06adbb5c3f09e8a7b740
Vijendharbhukya <93117322+Vijendharbhukya@users.noreply.github.com>
2021-12-06 18:07:13 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,3 +1,67 @@
+// import { useEffect } from "react";
+import EmpData from "./components/EmpData";
+import Header from "./components/Header";
+
+const App = () => {
+  console.log('App');
+  const num = 10;
+  const myData = { abc: 15, def: 25 }
+  const myArray = [10, 20, 30];
+
+  let dataFromChild = 0;
+
+  // useEffect(() => {
+  //   dataFromChild = 45;
+  // }, []);
+
+  const parentFun = (arg) => {
+    console.log('parentFun');
+    dataFromChild = arg(); // callback
+    console.log(dataFromChild);
+  }
+
+  // const aNormalFunction = () => {
+  //   console.log('normal funciton');
+  // }
+
+  return (
+    <div>
+      <Header />
+      <h1>CG React App</h1>
+      <p> {dataFromChild} </p>
+
+      {/* This will not work */}
+      {/* <p>{myData} </p> */}
+      {/* This will work  */}
+      <p>{myData.abc} </p>
+      <p>{myData.def} </p>
+      <p>{myArray}</p>
+      <p>{myArray[0]}</p>
+      <p>{num}</p>
+      <EmpData
+        // normalFun={aNormalFunction}
+        callParentFun={parentFun}
+        someData={myData}
+        numToPassToChild={num}
+        anotherData={myData}
+      />
+    </div>
+  );
+}
+export default App;
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>CG React App</h1>
+//       <p>Welcome to Capgemini React App...</p>
+//     </div>
+//   );
+// }
+// export default App;
+
+
+
 // import logo from './logo.svg';
 // import './App.css';
 
@@ -6,7 +70,7 @@
 //     <div className="App">
 //       <header className="App-header">
 //         <img src={logo} className="App-logo" alt="logo" />
-//         <p>Vijendhar
+//         <p>
 //           Edit <code>src/App.js</code> and save to reload.
 //         </p>
 //         <a
@@ -21,28 +85,4 @@
 //     </div>
 //   );
 // }
-
 // export default App;
-import logo from './logo.svg';
-
-import './App.css';
-
-
-
-function App() {
-
-  return (
-
-    <div className="App">
-
-      <p>Hello World</p>
-
-    </div>
-
-  );
-
-}
-
-
-
-export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+// types of components in react 
+// 1. function components 
+// 2. class components 
+
+
+// function component 
+// const Header = () => {
+
+//     return (
+//         <div className="display-4">
+//             <p>Header Component</p>
+//         </div>
+//     );
+// }
+// export default Header;
+
+// class component  
+import React from "react";
+class Header extends React.Component {
+
+    // constructor() { super(); }
+
+    render = () => {
+        return (
+            <div className="display-4" >
+                <p>Header Component</p>
+            </div>
+        )
+    }
+}
+export default Header;
