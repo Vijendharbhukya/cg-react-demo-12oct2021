ef7565800ab1636c909611aa9296d07eef24fef9
Vijendharbhukya <93117322+Vijendharbhukya@users.noreply.github.com>
2021-12-08 17:40:47 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,57 +1,65 @@
-
-// import { useState } from "react";
-import { useEffect, useState } from "react";
-import EmpData from "./components/EmpData";
-import Header from "./components/Header";
-import Hello from "./components/Hello";
+import Routes from "./Routes";
 
 const App = () => {
-  console.log('Parent component');
-  const num = 10;
-  const myData = { abc: 15, def: 25 }
-  const myArray = [10, 20, 30];
-
-  const [dataFromChild, setDataFromChild] = useState(4);
-
-  useEffect(() => {
-    console.log('parent useEffect');
-    setDataFromChild(5);
-  }, []);
-
-  const parentFun = (arg) => {
-    console.log('parentFun');
-    setDataFromChild(arg()); // callback
-    console.log(dataFromChild);
-  }
-
-
   return (
     <div>
-      <Header />
-      <div className="container">
-        <p className="display-4 text-primary" >CG React App</p>
-        <Hello />
-        {/* <p>Child data in parent component: {dataFromChild} </p> */}
-        {/* This will not work */}
-        {/* <p>{myData} </p> */}
-        {/* This will work  */}
-        {/* <p>{myData.abc} </p>
-        <p>{myData.def} </p>
-        <p>{myArray}</p>
-        <p>{myArray[0]}</p>
-        <p>{num}</p>
-        <EmpData
-          // normalFun={aNormalFunction}
-          callParentFun={parentFun}
-          someData={myData}
-          numToPassToChild={num}
-          anotherData={myData}
-        /> */}
-      </div>
-    </div>
+      <Routes />
+    </ div>
   );
 }
+
 export default App;
+
+
+//   console.log('Parent component');
+//   const num = 10;
+//   const myData = { abc: 15, def: 25 }
+//   const myArray = [10, 20, 30];
+
+//   const [dataFromChild, setDataFromChild] = useState(4);
+
+//   useEffect(() => {
+//     console.log('parent useEffect');
+//     setDataFromChild(5);
+//   }, []);
+
+//   const parentFun = (arg) => {
+//     console.log('parentFun');
+//     setDataFromChild(arg()); // callback
+//     console.log(dataFromChild);
+//   }
+
+
+//   return (
+//     <div>
+//       <Header />
+//       <div className="container">
+//         <p className="display-4 text-primary" >CG React App</p>
+//         <SpringBootData />
+//         {/* <Hello /> */}
+//         {/* <Login /> */}
+//         {/* <Register /> */}
+//         {/* <p>Child data in parent component: {dataFromChild} </p> */}
+//         {/* This will not work */}
+//         {/* <p>{myData} </p> */}
+//         {/* This will work  */}
+//         {/* <p>{myData.abc} </p>
+//         <p>{myData.def} </p>
+//         <p>{myArray}</p>
+//         <p>{myArray[0]}</p>
+//         <p>{num}</p>
+//         <EmpData
+//           // normalFun={aNormalFunction}
+//           callParentFun={parentFun}
+//           someData={myData}
+//           numToPassToChild={num}
+//           anotherData={myData}
+//         /> */}
+//       </div>
+//     </div>
+//   );
+// }
+// export default App;
 
 // function App() {
 //   return (
